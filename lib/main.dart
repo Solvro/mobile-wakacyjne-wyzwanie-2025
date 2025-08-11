264a29da24babdff2790bfd9ae3146247f8088c8
iza <iza.ola.paniczek@gmail.com>
2025-08-11 22:27:23 +0200

    rozwiazanie

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:hello_flutter/screens/list_view_screen.dart';
+
+void main() {
+  runApp(
+    MaterialApp(home: ListViewScreen(), debugShowCheckedModeBanner: false),
+  );
+}
