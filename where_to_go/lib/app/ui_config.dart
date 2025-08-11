59fbdb4ad442e397305792e8e05fdf27f9adcfde
Anna Sidor <annasidor00@gmail.com>
2025-08-11 10:05:46 +0200

    feat: complete week 1 - squashed

---
+++ where_to_go/lib/app/ui_config.dart
@@ -0,0 +1,19 @@
+abstract final class PlacesListScreenConfig {
+  static const imageSize = 60.0;
+  static const titleFontSize = 16.0;
+  static const radius = 15.0;
+}
+
+abstract final class DreamPlaceScreenConfig {
+  static const imageHeight = 200.0;
+  static const titleFontSize = 24.0;
+  static const descriptionFontSize = 16.0;
+  static const iconSize = 40.0;
+}
+
+abstract final class AppPaddings {
+  static const large = 16.0;
+  static const medium = 12.0;
+  static const small = 8.0;
+  static const tiny = 4.0;
+}
