d75d097a706c2f8befaf6042ee70ba3ae3a6939f
Jakub Czajkowski <and.jj.an@gmail.com>
2025-08-12 22:51:28 +0200

    feat: add codegen and first asset

--- where_to_go/lib/dream_place_screen.dart
+++ where_to_go/lib/dream_place_screen.dart
@@ -1,25 +1,32 @@
 import "package:flutter/material.dart";
 
+import "gen/assets.gen.dart";
+
 class DreamPlaceScreen extends StatelessWidget {
   const DreamPlaceScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFFFAFAFA),
-      appBar: AppBar(
-        title: const Text("Malaga"),
-      ),
-      body: const Center(
-        child: Text(
-          "Welcome to Malaga!",
-          style: TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
-            color: Color(0xFF141414),
-          ),
+        backgroundColor: const Color(0xFFFAFAFA),
+        appBar: AppBar(
+          title: const Text("Malaga"),
         ),
-      ),
-    );
+        body: Column(children: [
+          Image(
+            image: AssetImage(Assets.images.malaga.path),
+            fit: BoxFit.cover,
+          ),
+          const Center(
+            child: Text(
+              "Welcome to Malaga!",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Color(0xFF141414),
+              ),
+            ),
+          ),
+        ]));
   }
 }
