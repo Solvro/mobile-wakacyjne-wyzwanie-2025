c14ef4cd265f4be72aa56f891a2d8f9cf474fd9f
Jakub Czajkowski <and.jj.an@gmail.com>
2025-08-12 23:31:25 +0200

    feat: enhance DreamPlaceScreen with detailed header and description

--- where_to_go/lib/dream_place_screen.dart
+++ where_to_go/lib/dream_place_screen.dart
@@ -17,16 +17,37 @@ class DreamPlaceScreen extends StatelessWidget {
             image: AssetImage(Assets.images.malaga.path),
             fit: BoxFit.cover,
           ),
-          const Center(
-            child: Text(
-              "Welcome to Malaga!",
+          const DreamPlaceHeader()
+        ]));
+  }
+}
+
+class DreamPlaceHeader extends StatelessWidget {
+  const DreamPlaceHeader({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Malaga",
               style: TextStyle(
                 fontSize: 24,
                 fontWeight: FontWeight.bold,
                 color: Color(0xFF141414),
               ),
             ),
-          ),
-        ]));
+            SizedBox(
+              height: 8,
+            ),
+            Text(
+                "Malaga is a sunny city in southern Spain, known for its beautiful beaches, historic architecture, and Mediterranean climate.")
+          ],
+        ));
   }
 }
