adabc37a5c8129ab2cb21e7e72fa648b4dbde596
Ola Dominiak <ola@odominiak.com>
2025-08-14 16:21:59 +0200

    feat: implement list 1

--- where_to_go/lib/main.dart
+++ where_to_go/lib/main.dart
@@ -1,4 +1,6 @@
 import "package:flutter/material.dart";
+import "package:google_fonts/google_fonts.dart";
+import "gen/assets.gen.dart";
 
 void main() {
   runApp(const MyApp());
@@ -7,119 +9,260 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Flutter Demo",
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: "Flutter Demo Home Page"),
+      home: const ListScreen(),
+      theme: ThemeData(textTheme: GoogleFonts.latoTextTheme()),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
+class DreamPlace {
   final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  final String imagePath;
+  final String title2;
+  final String description;
+  final List<Iconka> ikony;
+  const DreamPlace(
+      {required this.title,
+      required this.title2,
+      required this.description,
+      required this.imagePath,
+      required this.ikony});
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+final listofplaces = [
+  DreamPlace(
+    title: "Mostar, Bośnia i Hercegowina",
+    title2: "Stary Most w Mostarze",
+    description: "Przy odrobinie szczęścia można spotkać osoby wykonujące ekstremalne skoki z mostu do rzeki.",
+    imagePath: Assets.images.mostar.path,
+    ikony: [const Iconka(icona: Icons.wallet, podpis: "cotoxd"), const Iconka(icona: Icons.wallet, podpis: "cotoxd")],
+  ),
+  DreamPlace(
+    title: "Wilno, Litwa",
+    title2: "Kościół św. Anny w Wilnie",
+    description:
+        "Jeden z najbardziej rozpoznawalnych symboli Wilna. Jeden w wielu pięknych kościołów do zobaczenia w tym mieście. ",
+    imagePath: Assets.images.wilno.path,
+    ikony: [const Iconka(icona: Icons.wallet, podpis: "cotoxd"), const Iconka(icona: Icons.wallet, podpis: "cotoxd")],
+  ),
+  DreamPlace(
+    title: "Berlin, Niemcy",
+    title2: "Brama Brandenburska",
+    description: "Najbardziej rozpoznawalny zabytek Berlina, obowiązkowy punkt każdej wizyty.",
+    imagePath: Assets.images.berlin.path,
+    ikony: [const Iconka(icona: Icons.wallet, podpis: "cotoxd"), const Iconka(icona: Icons.wallet, podpis: "cotoxd")],
+  ),
+  DreamPlace(
+    title: "Szklarska Poręba, Polska",
+    title2: "Wodospad Kamieńczyka",
+    description: "Najwyższy wodospoad w polskich Sudetach.",
+    imagePath: Assets.images.szklarska.path,
+    ikony: [const Iconka(icona: Icons.wallet, podpis: "cotoxd"), const Iconka(icona: Icons.wallet, podpis: "cotoxd")],
+  ),
+  DreamPlace(
+    title: "Bukareszt, Rumunia",
+    title2: "Pałac Parlamentu w Bukareszcie",
+    description: "Jeden z największych budynków na świecie, a zarazem symbol minionego komunizmu w Rumunii.",
+    imagePath: Assets.images.bukareszt.path,
+    ikony: [const Iconka(icona: Icons.wallet, podpis: "cotoxd"), const Iconka(icona: Icons.wallet, podpis: "cotoxd")],
+  )
+];
 
-  void _incrementCounter() {
+class Iconka {
+  final IconData icona;
+  final String podpis;
+
+  const Iconka({required this.icona, required this.podpis});
+}
+
+class ListScreen extends StatelessWidget {
+  const ListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: ListView(
+      children: listofplaces
+          .map(
+            (e) => Card(
+              child: ListTile(
+                onTap: () async {
+                  await Navigator.of(context).push(
+                    MaterialPageRoute<void>(builder: (context) => DreamPlaceScreen(place: e)),
+                  );
+                },
+                title: Text(e.title),
+              ),
+            ),
+          )
+          .toList(),
+    ));
+  }
+}
+
+class DreamPlaceScreen extends StatefulWidget {
+  const DreamPlaceScreen({super.key, required this.place});
+  final DreamPlace place;
+
+  @override
+  State<DreamPlaceScreen> createState() => _DreamPlaceScreenState();
+}
+
+class _DreamPlaceScreenState extends State<DreamPlaceScreen> {
+  bool _isFavorited = false;
+
+  void _toggleFavorite() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _isFavorited = !_isFavorited;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    const fioletowy = Color.fromARGB(255, 84, 26, 119);
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              "You have pushed the button this many times:",
-            ),
-            Text(
-              "$_counter",
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+        Image.asset(
+          widget.place.imagePath,
+          fit: BoxFit.cover,
         ),
+        Padding(
+            padding: const EdgeInsets.fromLTRB(20, 16, 20, 36),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  widget.place.title2,
+                  style: const TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                Text(
+                  widget.place.description,
+                  style: const TextStyle(fontSize: 15),
+                ),
+              ],
+            )),
+        Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: widget.place.ikony
+                .map(
+                  (e) => Column(
+                    children: [
+                      Icon(
+                        e.icona,
+                        size: 35,
+                        color: fioletowy,
+                      ),
+                      const SizedBox(
+                        height: 6,
+                      ),
+                      SizedBox(
+                        height: 40,
+                        child: Text(
+                          e.podpis,
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+                .toList()
+            // children: [
+            //   Column(
+            //     children: [
+            //       Icon(
+            //         Icons.water,
+            //         size: 35,
+            //         color: fioletowy,
+            //       ),
+            //       SizedBox(
+            //         height: 6,
+            //       ),
+            //       SizedBox(
+            //         height: 40,
+            //         child: Text(
+            //           "Rzeka\nNeretwa",
+            //           textAlign: TextAlign.center,
+            //         ),
+            //       ),
+            //     ],
+            //   ),
+            //   Column(
+            //     children: [
+            //       Icon(
+            //         Icons.landscape,
+            //         size: 35,
+            //         color: Color.fromARGB(255, 84, 26, 119),
+            //       ),
+            //       SizedBox(
+            //         height: 6,
+            //       ),
+            //       SizedBox(
+            //         height: 40,
+            //         child: Text("Widoczki"),
+            //       ),
+            //     ],
+            //   ),
+            //   Column(
+            //     children: [
+            //       Icon(
+            //         Icons.storefront,
+            //         size: 35,
+            //         color: Color.fromARGB(255, 84, 26, 119),
+            //       ),
+            //       SizedBox(
+            //         height: 8,
+            //       ),
+            //       SizedBox(
+            //         height: 40,
+            //         child: Text(
+            //           "Tradycyjne\nbudownictwo",
+            //           textAlign: TextAlign.center,
+            //         ),
+            //       ),
+            //     ],
+            //   ),
+            //   Column(
+            //     children: [
+            //       Icon(
+            //         Icons.mosque,
+            //         size: 35,
+            //         color: Color.fromARGB(255, 84, 26, 119),
+            //       ),
+            //       SizedBox(
+            //         height: 6,
+            //       ),
+            //       SizedBox(
+            //         height: 40,
+            //         child: Text("Meczety"),
+            //       ),
+            //     ],
+            //   ),
+            // ],
+            )
+      ]),
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text(widget.place.title),
+        titleTextStyle: const TextStyle(
+          fontSize: 18,
+        ),
+        centerTitle: true,
+        backgroundColor: const Color.fromARGB(255, 84, 26, 119),
+        actions: [
+          IconButton(
+              onPressed: _toggleFavorite,
+              icon: Icon(
+                _isFavorited ? Icons.favorite_rounded : Icons.favorite_border_rounded,
+                color: _isFavorited ? const Color.fromARGB(255, 174, 14, 14) : Colors.white,
+              ))
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: "Increment",
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
