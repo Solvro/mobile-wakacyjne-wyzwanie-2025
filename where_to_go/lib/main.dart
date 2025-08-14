8b2a6bd62e60e69c090a2c142806003d6a85cc20
Twoje Imie Nazwisko <twoj_email@example.com>
2025-08-14 19:35:52 +0200

    Dart format

--- where_to_go/lib/main.dart
+++ where_to_go/lib/main.dart
@@ -6,15 +6,16 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-    const MyApp({super.key});
+  const MyApp({super.key});
 
-    @override
-    Widget build(BuildContext context) {
-        return const MaterialApp(
-          home: PlaceListScreen(),
-        );
-    }
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: PlaceListScreen(),
+    );
+  }
 }
+
 class Place {
   final String placeName;
   final String placeDescription;
@@ -29,115 +30,101 @@ class PlaceListScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final List<Place> places = [
-      Place (
+      Place(
         placeName: "Sagrada Familia",
-        placeDescription: "Jeden z najbardziej rozpoznawalnych budynków na świecie - dzieło życia architekta, Antoniego Gaudiego. Jego budowa trwa od 1882 roku (143 lata), a termin jej ukończenia jest wciąż nieznany.",
+        placeDescription:
+            "Jeden z najbardziej rozpoznawalnych budynków na świecie - dzieło życia architekta, Antoniego Gaudiego. Jego budowa trwa od 1882 roku (143 lata), a termin jej ukończenia jest wciąż nieznany.",
         placeImage: Assets.images.sagrada.path,
       ),
-      Place (
+      Place(
         placeName: "Park Guell",
-        placeDescription: "Nietypowy park zaprojektowany przez Antoniego Gaudiego z pięknym widokiem na panoramę Barcelony. Teren zachwyca różnorodnością, nietypowym podejściem do kształtowania przestrzeni oraz mozaikami z potłuczonych kawałków ceramicznych płytek.",
+        placeDescription:
+            "Nietypowy park zaprojektowany przez Antoniego Gaudiego z pięknym widokiem na panoramę Barcelony. Teren zachwyca różnorodnością, nietypowym podejściem do kształtowania przestrzeni oraz mozaikami z potłuczonych kawałków ceramicznych płytek.",
         placeImage: Assets.images.park.path,
       ),
-      Place (
+      Place(
         placeName: "Pałac Muzyki Katalońskiej",
-        placeDescription: "Sala koncertowa z wielobarwną, witrażową kopółą przez którą do środka dociera światło o różnym natężeniu i kolorze w zależności od pory dnia. Równie interesujący jest front budynku, który ozdabiają kolumny pokryte mozaiką tworzącą kwieciste wzory.",
+        placeDescription:
+            "Sala koncertowa z wielobarwną, witrażową kopółą przez którą do środka dociera światło o różnym natężeniu i kolorze w zależności od pory dnia. Równie interesujący jest front budynku, który ozdabiają kolumny pokryte mozaiką tworzącą kwieciste wzory.",
         placeImage: Assets.images.palac.path,
       ),
-      Place (
+      Place(
         placeName: "Camp Nou",
-        placeDescription: "Stadion piłkarski klubu FC Barcelona. Jego trybuny mieszczą 99 354 osób (obecnie jest przebudowywany przez co jego pojemność wzrośnie do 105 000 osób), co czyni go największym piłkarskim stadionem w Europie i jednym z największych na świecie. ",
+        placeDescription:
+            "Stadion piłkarski klubu FC Barcelona. Jego trybuny mieszczą 99 354 osób (obecnie jest przebudowywany przez co jego pojemność wzrośnie do 105 000 osób), co czyni go największym piłkarskim stadionem w Europie i jednym z największych na świecie. ",
         placeImage: Assets.images.camp.path,
       ),
-      Place (
+      Place(
         placeName: "Casa Batlo",
-        placeDescription: "Budynek mieszkalny, przebudowany przez Antoniego Gaudiego w 1904 roku na zlecenie prodzucenta tekstyliów Josepha Batlo. Budynek jest bogato zdobiony, elewacja zawiera elementy zwierzęce jak np. łuski smoka.",
+        placeDescription:
+            "Budynek mieszkalny, przebudowany przez Antoniego Gaudiego w 1904 roku na zlecenie prodzucenta tekstyliów Josepha Batlo. Budynek jest bogato zdobiony, elewacja zawiera elementy zwierzęce jak np. łuski smoka.",
         placeImage: Assets.images.casa.path,
       )
     ];
 
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 196, 194, 194),
-      appBar: AppBar(
-        title: const Text(
+        backgroundColor: const Color.fromARGB(255, 196, 194, 194),
+        appBar: AppBar(
+            title: const Text(
           "Hiszpania, Barcelona",
-        )
-      ),
-      body: ListView.builder(
-        itemCount: places.length,
-        itemBuilder: (context, index) {
-          final place = places[index];
-          return InkWell(
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => DreamPlaceScreen(
-                    myPlaceName: place.placeName,
-                    myPlaceDescription: place.placeDescription,
-                    myPlaceImage: place.placeImage,
-                  ),
-                ),
-              );
-            },
-            child: Card(
-              clipBehavior: Clip.antiAlias,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(12),
-              ),
-              margin: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
-              child: Padding(
-                padding: const EdgeInsets.all(8),
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children:[
-                    ClipRRect(
-                      borderRadius: BorderRadius.circular(8),
-                      child: Image.asset(
-                        place.placeImage, 
-                        height: 150,
-                        fit: BoxFit.cover,
-                      )
-                    ),
-                    const SizedBox(width: 12),
-                    Expanded(
-                      child: ListTile (
-                        title: Text(
-                          place.placeName,
-                          style: const TextStyle(
-                            fontSize: 18,
-                            color: Colors.black,
-                            fontWeight: FontWeight.bold
-                          )
+        )),
+        body: ListView.builder(
+            itemCount: places.length,
+            itemBuilder: (context, index) {
+              final place = places[index];
+              return InkWell(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => DreamPlaceScreen(
+                          myPlaceName: place.placeName,
+                          myPlaceDescription: place.placeDescription,
+                          myPlaceImage: place.placeImage,
                         ),
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => DreamPlaceScreen(
-                                myPlaceName: place.placeName,
-                                myPlaceDescription: place.placeDescription,
-                                myPlaceImage: place.placeImage,
-                              )
-                            )
-                          );
-                        }
-                      )
-                    )
-                  ]
-                )
-              )
-            )
-          );
-        }
-      ) 
-    );
+                      ),
+                    );
+                  },
+                  child: Card(
+                      clipBehavior: Clip.antiAlias,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      margin: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
+                      child: Padding(
+                          padding: const EdgeInsets.all(8),
+                          child: Row(mainAxisSize: MainAxisSize.min, children: [
+                            ClipRRect(
+                                borderRadius: BorderRadius.circular(8),
+                                child: Image.asset(
+                                  place.placeImage,
+                                  height: 150,
+                                  fit: BoxFit.cover,
+                                )),
+                            const SizedBox(width: 12),
+                            Expanded(
+                                child: ListTile(
+                                    title: Text(place.placeName,
+                                        style: const TextStyle(
+                                            fontSize: 18, color: Colors.black, fontWeight: FontWeight.bold)),
+                                    onTap: () {
+                                      Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                              builder: (context) => DreamPlaceScreen(
+                                                    myPlaceName: place.placeName,
+                                                    myPlaceDescription: place.placeDescription,
+                                                    myPlaceImage: place.placeImage,
+                                                  )));
+                                    }))
+                          ]))));
+            }));
   }
 }
 
-
 class DreamPlaceScreen extends StatefulWidget {
-  const DreamPlaceScreen({super.key, required this.myPlaceName, required this.myPlaceDescription, required this.myPlaceImage});
+  const DreamPlaceScreen(
+      {super.key, required this.myPlaceName, required this.myPlaceDescription, required this.myPlaceImage});
 
   final String myPlaceName;
   final String myPlaceDescription;
@@ -148,131 +135,85 @@ class DreamPlaceScreen extends StatefulWidget {
 }
 
 class _DreamPlaceScreenState extends State<DreamPlaceScreen> {
-
   _DreamPlaceScreenState(this.myPlaceName, this.myPlaceDescription, this.myPlaceImage);
 
   bool _isFavorited = false;
   final String myPlaceName;
   final String myPlaceDescription;
   final String myPlaceImage;
-  
+
   void toggleFavorite() {
     setState(() {
       _isFavorited = !_isFavorited;
     });
   }
 
-  Column bottomRowColumn(IconData iconType, Color iconColor, String iconText)
-  {
-    return Column (
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children:[
-        Icon(
-          iconType,
-          color: iconColor,
-          size: 40,
-        ),
-        Text( 
-          iconText,
-          style: const TextStyle(
-            fontSize: 10,
-            color: Colors.black,
-            fontWeight: FontWeight.bold
-          )
-        )
-      ]
-    );
+  Column bottomRowColumn(IconData iconType, Color iconColor, String iconText) {
+    return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+      Icon(
+        iconType,
+        color: iconColor,
+        size: 40,
+      ),
+      Text(iconText, style: const TextStyle(fontSize: 10, color: Colors.black, fontWeight: FontWeight.bold))
+    ]);
   }
-  
 
   @override
-  Widget build(BuildContext context) 
-  {
+  Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 196, 194, 194),
-      appBar: AppBar(
-        title: const Text(
-          "Barcelona, Hiszpania"
-          ),
-        actions: [
-          IconButton(
-            icon: Icon(
-              _isFavorited ? Icons.favorite : Icons.favorite_border
-              ),
-            onPressed: toggleFavorite
-          )
-        ],
-      ),
-      body: Column(
-        children: [
+        backgroundColor: const Color.fromARGB(255, 196, 194, 194),
+        appBar: AppBar(
+          title: const Text("Barcelona, Hiszpania"),
+          actions: [
+            IconButton(icon: Icon(_isFavorited ? Icons.favorite : Icons.favorite_border), onPressed: toggleFavorite)
+          ],
+        ),
+        body: Column(children: [
           Image.asset(
             myPlaceImage,
             fit: BoxFit.cover,
           ),
-
           Padding(
-            padding: const EdgeInsets.all(16),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
+              padding: const EdgeInsets.all(16),
+              child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
                 const SizedBox(height: 8),
-                Text(
-                  myPlaceName,
-                  style: const TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
-                  )
-                ),
-
+                Text(myPlaceName,
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    )),
                 const SizedBox(height: 8),
-                Text(
-                 myPlaceDescription,
-                  style: const TextStyle(
-                    fontSize: 12,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black
-                  )
-                ),
+                Text(myPlaceDescription,
+                    style: const TextStyle(fontSize: 12, fontWeight: FontWeight.bold, color: Colors.black)),
                 const SizedBox(height: 12),
-
-                 Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children:[
-                    bottomRowColumn(Icons.wb_sunny,const Color.fromARGB(255, 186, 144, 20), "pogoda"),
-                    bottomRowColumn(Icons.map,const Color.fromARGB(255, 31, 41, 61), "zabytki"),
-                    bottomRowColumn(Icons.restaurant,const Color.fromARGB(255, 70, 19, 2), "jedzenie"),
-                  ]
-                )
-              ]
-            )
-          )
-        ] 
-      )
-    );
-  } 
+                Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+                  bottomRowColumn(Icons.wb_sunny, const Color.fromARGB(255, 186, 144, 20), "pogoda"),
+                  bottomRowColumn(Icons.map, const Color.fromARGB(255, 31, 41, 61), "zabytki"),
+                  bottomRowColumn(Icons.restaurant, const Color.fromARGB(255, 70, 19, 2), "jedzenie"),
+                ])
+              ]))
+        ]));
+  }
 }
 
-
-
-
-      
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        /*colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+// This is the theme of your application.
+//
+// TRY THIS: Try running your application with "flutter run". You'll see
+// the application has a purple toolbar. Then, without quitting the app,
+// try changing the seedColor in the colorScheme below to Colors.green
+// and then invoke "hot reload" (save your changes or press the "hot
+// reload" button in a Flutter-supported IDE, or press "r" if you used
+// the command line to start the app).
+//
+// Notice that the counter didn't reset back to zero; the application
+// state is not lost during the reload. To reset the state, use hot
+// restart instead.
+//
+// This works for code too, not just values: Most code changes can be
+// tested with just a hot reload.
+/*colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: "Flutter Demo Home Page"),
