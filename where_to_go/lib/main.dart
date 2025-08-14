18212e05893f901f820fa929b7f1649bff2d4c5d
Hanzoze <danilchenko.ilya2005@gmail.com>
2025-08-14 18:51:45 +0200

    trying to fix again

--- where_to_go/lib/main.dart
+++ where_to_go/lib/main.dart
@@ -1,6 +1,7 @@
-import 'package:flutter/material.dart';
-import 'gen/assets.gen.dart';
-import 'package:google_fonts/google_fonts.dart';
+import "package:flutter/material.dart";
+import "package:google_fonts/google_fonts.dart";
+
+import "gen/assets.gen.dart";
 
 void main() {
   runApp(const MyApp());
@@ -15,8 +16,8 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
           textTheme: GoogleFonts.robotoTextTheme(),
-          appBarTheme: AppBarTheme(
-            backgroundColor: const Color.fromARGB(255, 177, 208, 223),
+          appBarTheme: const AppBarTheme(
+            backgroundColor: Color.fromARGB(255, 177, 208, 223),
             titleTextStyle: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
           ),
         ),
@@ -47,55 +48,50 @@ class Place {
 class PlaceScreenList extends StatelessWidget {
   final List<Place> places = [
     Place(
-      titleText: 'Cool place',
+      titleText: "Cool place",
       image: Assets.images.snowdin.provider(),
-      placeName:
-          Center(child: const Text('Snowdin', style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold))),
-      catchPhrase: Center(child: const Text('here lives the GREAT Papyrus')),
-      feature1: Column(children: const [Icon(Icons.wb_sunny), Text('No sun')]),
-      feature2: Column(children: const [Icon(Icons.beach_access), Text('No beach')]),
-      feature3: Column(children: const [Icon(Icons.restaurant), Text('Tasty pasta')]),
+      placeName: const Center(child: Text("Snowdin", style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold))),
+      catchPhrase: const Center(child: Text("here lives the GREAT Papyrus")),
+      feature1: const Column(children: [Icon(Icons.wb_sunny), Text("No sun")]),
+      feature2: const Column(children: [Icon(Icons.beach_access), Text("No beach")]),
+      feature3: const Column(children: [Icon(Icons.restaurant), Text("Tasty pasta")]),
     ),
     Place(
-      titleText: 'The longest city in Europe',
+      titleText: "The longest city in Europe",
       image: Assets.images.kr.provider(),
-      placeName:
-          Center(child: const Text('Kryvyi Rih', style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold))),
-      catchPhrase: Center(child: const Text('A life-long city')),
-      feature1: Column(children: const [Icon(Icons.iron), Text('Metals!')]),
-      feature2: Column(children: const [Icon(Icons.pets), Text('Green dogs')]),
-      feature3: Column(children: const [Icon(Icons.do_not_disturb), Text('Less criminal then before')]),
+      placeName: const Center(child: Text("Kryvyi Rih", style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold))),
+      catchPhrase: const Center(child: Text("A life-long city")),
+      feature1: const Column(children: [Icon(Icons.iron), Text("Metals!")]),
+      feature2: const Column(children: [Icon(Icons.pets), Text("Green dogs")]),
+      feature3: const Column(children: [Icon(Icons.do_not_disturb), Text("Less criminal then before")]),
     ),
     Place(
-      titleText: 'Big snowy mountains',
+      titleText: "Big snowy mountains",
       image: Assets.images.zakopane.provider(),
-      placeName:
-          Center(child: const Text('Zakopane', style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold))),
-      catchPhrase: Center(child: const Text('Zakopane - najbliżej Tatr')),
-      feature1: Column(children: const [Icon(Icons.snowboarding), Text('Snow sports!')]),
-      feature2: Column(children: const [Icon(Icons.hiking), Text('Cool routes to go hiking')]),
-      feature3: Column(children: const [Icon(Icons.water), Text('Oko morskie!')]),
+      placeName: const Center(child: Text("Zakopane", style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold))),
+      catchPhrase: const Center(child: Text("Zakopane - najbliżej Tatr")),
+      feature1: const Column(children: [Icon(Icons.snowboarding), Text("Snow sports!")]),
+      feature2: const Column(children: [Icon(Icons.hiking), Text("Cool routes to go hiking")]),
+      feature3: const Column(children: [Icon(Icons.water), Text("Oko morskie!")]),
     ),
     Place(
-      titleText: 'GAMBLING',
+      titleText: "GAMBLING",
       image: Assets.images.gambling.provider(),
-      placeName:
-          Center(child: const Text('Las Vegas', style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold))),
-      catchPhrase: Center(child: const Text('LET`S GO GAMBLING')),
-      feature1: Column(children: const [Icon(Icons.money_off), Text('GAMBLING')]),
-      feature2: Column(children: const [Icon(Icons.attach_money), Text('GAMBLING')]),
-      feature3: Column(children: const [Icon(Icons.monetization_on), Text('GAMBLING')]),
+      placeName: const Center(child: Text("Las Vegas", style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold))),
+      catchPhrase: const Center(child: Text("LET`S GO GAMBLING")),
+      feature1: const Column(children: [Icon(Icons.money_off), Text("GAMBLING")]),
+      feature2: const Column(children: [Icon(Icons.attach_money), Text("GAMBLING")]),
+      feature3: const Column(children: [Icon(Icons.monetization_on), Text("GAMBLING")]),
     ),
     Place(
-      titleText: 'I need to get there',
+      titleText: "I need to get there",
       image: Assets.images.pentagon.provider(),
-      placeName:
-          Center(child: const Text('Pentagon', style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold))),
-      catchPhrase: Center(child: const Text('You know they built it in 1943?')),
-      feature1: Column(children: const [Icon(Icons.house), Text('5 sides')]),
-      feature2: Column(children: const [Icon(Icons.build), Text('Looks cozy')]),
-      feature3: Column(
-          children: const [Icon(Icons.build_circle), Text('5 aboveground floors,\nand 2 underground,\nofficially')]),
+      placeName: const Center(child: Text("Pentagon", style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold))),
+      catchPhrase: const Center(child: Text("You know they built it in 1943?")),
+      feature1: const Column(children: [Icon(Icons.house), Text("5 sides")]),
+      feature2: const Column(children: [Icon(Icons.build), Text("Looks cozy")]),
+      feature3: const Column(
+          children: [Icon(Icons.build_circle), Text("5 aboveground floors,\nand 2 underground,\nofficially")]),
     ),
   ];
 
@@ -103,7 +99,7 @@ class PlaceScreenList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Dream places'),
+        title: const Text("Dream places"),
       ),
       body: ListView.builder(
           itemCount: places.length,
@@ -111,7 +107,8 @@ class PlaceScreenList extends StatelessWidget {
             final place = places[index];
             return InkWell(
                 onTap: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (_) => DreamPlaceScreen(place: place)));
+                  Navigator.push<void>(
+                      context, MaterialPageRoute<void>(builder: (_) => DreamPlaceScreen(place: place)));
                 },
                 child: Card(
                   margin: const EdgeInsets.all(8),
@@ -119,7 +116,7 @@ class PlaceScreenList extends StatelessWidget {
                     children: [
                       Image(image: place.image, height: 200, fit: BoxFit.cover),
                       Padding(
-                        padding: const EdgeInsets.all(8.0),
+                        padding: const EdgeInsets.all(8),
                         child: Text(place.titleText, style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
                       ),
                     ],
@@ -155,19 +152,21 @@ class _DreamPlaceScreenState extends State<DreamPlaceScreen> {
       appBar: AppBar(
         title: Text(widget.place.titleText),
         actions: [
-          IconButton(onPressed: _toggleFavorite, icon: _isFavored ? Icon(Icons.favorite) : Icon(Icons.favorite_border))
+          IconButton(
+              onPressed: _toggleFavorite,
+              icon: _isFavored ? const Icon(Icons.favorite) : const Icon(Icons.favorite_border))
         ],
       ),
       body: Column(
         children: [
           Image(image: widget.place.image, height: 250, fit: BoxFit.cover),
           Padding(
-            padding: const EdgeInsets.all(16.0),
+            padding: const EdgeInsets.all(16),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 widget.place.placeName,
-                SizedBox(
+                const SizedBox(
                   height: 8,
                 ),
                 widget.place.catchPhrase
