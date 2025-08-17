a0d1652f2f93218c4ebcaa61db09b2ba49e08198
Wojciech Dominiak <wojciechdominiak80@gmail.com>
2025-08-17 22:23:56 +0200

    feat: add title and description (task 3)

--- where_to_go/lib/main.dart
+++ where_to_go/lib/main.dart
@@ -32,6 +32,35 @@ class DreamPlaceScreen extends StatelessWidget {
           Assets.images.santorini.image(
             fit: BoxFit.cover
           ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Perła Cyklad na Morzu Egejskim',
+                  style: TextStyle(
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blueGrey[800],
+                  ),
+                ),
+
+                const SizedBox(height: 8),
+
+                const Text(
+                  'Odkryj magiczne białe domki zawieszone na klifach i zanurz '
+                      'się w błękicie morza, podziwiając najpiękniejsze zachody '
+                      'słońca na świecie.',
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    height: 1.4,
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
