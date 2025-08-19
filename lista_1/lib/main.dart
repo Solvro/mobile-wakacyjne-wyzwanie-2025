0a9a95ad2b4440742f38a4e70627b7e6dfec39be
iza <iza.ola.paniczek@gmail.com>
2025-08-19 11:06:38 +0200

    changes in folder

--- lista_1/lib/main.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:hello_flutter/screens/list_view_screen.dart';
-
-void main() {
-  runApp(
-    MaterialApp(home: ListViewScreen(), debugShowCheckedModeBanner: false),
-  );
-}
