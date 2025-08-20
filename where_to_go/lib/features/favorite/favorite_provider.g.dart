f584f5753cd8a28d9d868a06d54f6284fc811cb9
Maxoo005 <maksymiliankula@wp.pl>
2025-08-21 00:40:20 +0200

    lista2-zad3-dołączam .g.dart

---
+++ where_to_go/lib/features/favorite/favorite_provider.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'favorite_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$favoriteHash() => r'c06f83e2e5d8a6745504344d8597bc8b4f2c2739';
+
+/// See also [Favorite].
+@ProviderFor(Favorite)
+final favoriteProvider = AutoDisposeNotifierProvider<Favorite, bool>.internal(
+  Favorite.new,
+  name: r'favoriteProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$favoriteHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$Favorite = AutoDisposeNotifier<bool>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
