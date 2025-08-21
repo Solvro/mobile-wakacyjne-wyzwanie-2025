1fdd81ef214cade93538bef04a84f868275310e8
blrhn <mm9854815@gmail.com>
2025-08-21 14:29:17 +0200

    fix

--- where_to_go/lib/features/favorite/favorite_provider.dart
+++ where_to_go/lib/features/favorite/favorite_provider.dart
@@ -5,7 +5,8 @@ part "favorite_provider.g.dart";
 @riverpod
 class Favorite extends _$Favorite {
   @override
-  bool build(/*Place place*/) { // z ciekawosci
+  bool build(/*Place place*/) {
+    // z ciekawosci
     //return place.isFavorite;
     return false;
   }
