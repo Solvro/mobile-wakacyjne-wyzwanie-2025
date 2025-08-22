77d11854fa9513b829adc6970bfa1c3e5b53e0bf
Grzegorz Diaconescu <grzes.business@gmail.com>
2025-08-22 13:27:44 +0200

    Implemented Dark/Light mode functionality

--- where_to_go/lib/dream_place_screen.dart
+++ where_to_go/lib/dream_place_screen.dart
@@ -1,6 +1,8 @@
 import "package:flutter/material.dart";
 import "package:flutter_riverpod/flutter_riverpod.dart";
 import "features/places/places_provider.dart";
+import "features/theme/local_theme_provider.dart";
+import "features/theme/theme.dart";
 
 class DreamPlaceScreen extends ConsumerWidget {
   final String id;
@@ -12,69 +14,86 @@ class DreamPlaceScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final isFavourited = ref.watch(placesProvider.select((places) => places.firstWhere((p) => p.id == id).isFavorite));
-    final icon = isFavourited ? Icons.favorite : Icons.favorite_border;
+    final themeAsync = ref.watch(localThemeNotifierProvider);
+    return themeAsync.when(
+      data: (currentTheme) {
+        final themePalette = ThemePalette();
+        final isFavourited =
+            ref.watch(placesProvider.select((places) => places.firstWhere((p) => p.id == id).isFavorite));
+        final icon = isFavourited ? Icons.favorite : Icons.favorite_border;
 
-    final places = ref.watch(placesProvider);
-    final place = places.firstWhere((p) => p.id == id);
+        final places = ref.watch(placesProvider);
+        final place = places.firstWhere((p) => p.id == id);
 
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(place.name),
-        actions: [
-          IconButton(
-            icon: Icon(
-              icon,
-              color: isFavourited ? Colors.red : Colors.black,
-              size: 28,
+        return Scaffold(
+          backgroundColor: themePalette.getPrimaryColor(currentTheme, context),
+          appBar: AppBar(
+            iconTheme: IconThemeData(
+              color: themePalette.getSecondaryColor(currentTheme, context), // Set back button color
             ),
-            onPressed: () async {
-              await ref.read(placesProvider.notifier).toggleFavorite(id);
-            },
-            style: OutlinedButton.styleFrom(
-              foregroundColor: Colors.white,
-            ),
+            backgroundColor: themePalette.getPrimaryColor(currentTheme, context),
+            title: Text(place.name, style: TextStyle(color: themePalette.getSecondaryColor(currentTheme, context))),
+            actions: [
+              IconButton(
+                icon: Icon(
+                  icon,
+                  color: isFavourited ? Colors.red : themePalette.getSecondaryColor(currentTheme, context),
+                  size: 28,
+                ),
+                onPressed: () async {
+                  await ref.read(placesProvider.notifier).toggleFavorite(id);
+                },
+                style: OutlinedButton.styleFrom(
+                  foregroundColor: themePalette.getPrimaryColor(currentTheme, context),
+                ),
+              ),
+            ],
           ),
-        ],
-      ),
-      body: Column(
-        children: [
-          place.image.image(
-            width: double.infinity,
-            fit: BoxFit.cover,
+          body: Column(
+            children: [
+              place.image.image(
+                width: double.infinity,
+                fit: BoxFit.cover,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(place.name,
+                        style: TextStyle(
+                          fontSize: 28,
+                          fontWeight: FontWeight.bold,
+                          color: ThemePalette().secondaryColor,
+                        )),
+                    const SizedBox(height: 8),
+                    Text(place.description,
+                        style: TextStyle(color: themePalette.getSecondaryColor(currentTheme, context))),
+                  ],
+                ),
+              ),
+              const Spacer(),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 36),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: place.attractions.entries
+                      .map((entry) => Column(
+                            children: [
+                              Icon(entry.value, color: themePalette.getSecondaryColor(currentTheme, context)),
+                              Text(entry.key,
+                                  style: TextStyle(color: themePalette.getSecondaryColor(currentTheme, context))),
+                            ],
+                          ))
+                      .toList(),
+                ),
+              ),
+            ],
           ),
-          Padding(
-            padding: const EdgeInsets.all(16),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(place.name,
-                    style: const TextStyle(
-                      fontSize: 28,
-                      fontWeight: FontWeight.bold,
-                    )),
-                const SizedBox(height: 8),
-                Text(place.description),
-              ],
-            ),
-          ),
-          const Spacer(),
-          Padding(
-            padding: const EdgeInsets.only(bottom: 36),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: place.attractions.entries
-                  .map((entry) => Column(
-                        children: [
-                          Icon(entry.value),
-                          Text(entry.key),
-                        ],
-                      ))
-                  .toList(),
-            ),
-          ),
-        ],
-      ),
+        );
+      },
+      loading: () => const Center(child: CircularProgressIndicator()),
+      error: (err, stack) => const Center(child: Text("Error loading theme")),
     );
   }
 }
--- where_to_go/lib/home_screen.dart
+++ where_to_go/lib/home_screen.dart
@@ -2,12 +2,17 @@ import "package:flutter/material.dart";
 import "package:flutter_riverpod/flutter_riverpod.dart";
 import "package:go_router/go_router.dart";
 import "features/places/places_provider.dart";
+import "features/theme/local_theme_provider.dart";
+import "features/theme/local_theme_repository.dart";
+import "features/theme/theme.dart" show ThemePalette;
 import "gen/assets.gen.dart";
 
 class HomeScreen extends ConsumerWidget {
-  const HomeScreen();
+  HomeScreen();
 
-  Widget getImageWidget(dynamic imageSource) {
+  final palette = ThemePalette();
+
+  Widget getImageWidget(dynamic imageSource, LocalTheme currentTheme, BuildContext context) {
     if (imageSource is String) {
       return Image.asset(
         imageSource,
@@ -20,72 +25,110 @@ class HomeScreen extends ConsumerWidget {
         fit: BoxFit.cover,
       );
     }
-    return const ColoredBox(
-      color: Colors.grey,
-      child: Icon(Icons.image_not_supported, size: 50),
+    return ColoredBox(
+      color: palette.getSecondaryColor(currentTheme, context),
+      child: const Icon(Icons.image_not_supported, size: 50),
     );
   }
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final places = ref.watch(placesProvider);
+    final themeAsync = ref.watch(localThemeNotifierProvider);
+    return themeAsync.when(
+      data: (currentTheme) {
+        final icon = currentTheme == LocalTheme.light ? Icons.light_mode : Icons.dark_mode;
 
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text("My Favorite Places", style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold)),
-        ),
-        body: GridView.count(
-            primary: false,
-            padding: const EdgeInsets.all(20),
-            crossAxisSpacing: 10,
-            mainAxisSpacing: 10,
-            crossAxisCount: 2,
-            children: [
-              for (final place in places)
-                GestureDetector(
-                  onTap: () async {
-                    await context.push("/details/${place.id}");
-                  },
-                  child: Card(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12),
+        return AnimatedSwitcher(
+            duration: const Duration(milliseconds: 200),
+            child: Scaffold(
+                key: ValueKey(currentTheme),
+                backgroundColor: palette.getPrimaryColor(currentTheme, context),
+                appBar: AppBar(
+                  title: Text("My Favorite Places",
+                      style: TextStyle(
+                          fontSize: 32,
+                          fontWeight: FontWeight.bold,
+                          color: palette.getSecondaryColor(currentTheme, context))),
+                  actions: [
+                    IconButton(
+                      icon: Icon(
+                        icon,
+                        color: palette.getSecondaryColor(currentTheme, context),
+                        size: 28,
+                      ),
+                      onPressed: () async {
+                        await ref.read(localThemeNotifierProvider.notifier).toggleTheme();
+                      },
+                      style: OutlinedButton.styleFrom(
+                        foregroundColor: palette.getPrimaryColor(currentTheme, context),
+                      ),
                     ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Expanded(
-                          child: ClipRRect(
-                            borderRadius: const BorderRadius.vertical(top: Radius.circular(12)),
-                            child: place.image.image(
-                              width: double.infinity,
-                              fit: BoxFit.cover,
+                  ],
+                  backgroundColor: palette.getPrimaryColor(currentTheme, context),
+                ),
+                body: GridView.count(
+                    primary: false,
+                    padding: const EdgeInsets.all(20),
+                    crossAxisSpacing: 10,
+                    mainAxisSpacing: 10,
+                    crossAxisCount: 2,
+                    children: [
+                      for (final place in places)
+                        GestureDetector(
+                          onTap: () async {
+                            await context.push("/details/${place.id}");
+                          },
+                          child: Card(
+                            color: palette.getPrimaryColor(currentTheme, context),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Expanded(
+                                  child: ClipRRect(
+                                    borderRadius: const BorderRadius.vertical(top: Radius.circular(12)),
+                                    child: place.image.image(
+                                      width: double.infinity,
+                                      fit: BoxFit.cover,
+                                    ),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(8),
+                                  child: Row(
+                                    children: [
+                                      Expanded(
+                                        child: Text(
+                                          place.name,
+                                          style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            color: palette.getSecondaryColor(currentTheme, context),
+                                          ),
+                                          maxLines: 1,
+                                          overflow: TextOverflow.ellipsis,
+                                        ),
+                                      ),
+                                      Icon(
+                                        place.isFavorite ? Icons.favorite : Icons.favorite_border,
+                                        color: place.isFavorite
+                                            ? Colors.red
+                                            : palette.getSecondaryColor(currentTheme, context),
+                                        size: 20,
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
                             ),
                           ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(8),
-                          child: Row(
-                            children: [
-                              Expanded(
-                                child: Text(
-                                  place.name,
-                                  style: const TextStyle(fontWeight: FontWeight.bold),
-                                  maxLines: 1,
-                                  overflow: TextOverflow.ellipsis,
-                                ),
-                              ),
-                              Icon(
-                                place.isFavorite ? Icons.favorite : Icons.favorite_border,
-                                color: place.isFavorite ? Colors.red : null,
-                                size: 20,
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                )
-            ].toList()));
+                        )
+                    ].toList())));
+      },
+      loading: () => const Center(child: CircularProgressIndicator()),
+      error: (err, stack) => const Center(child: Text("Error loading theme")),
+    );
   }
 }
