93df0b170b58c222c2aa39770f4cb6801b998594
Wojciech Dominiak <wojciechdominiak80@gmail.com>
2025-08-23 22:15:45 +0200

    refactor: wrap app with ProviderScope for Riverpod

--- where_to_go/lib/main.dart
+++ where_to_go/lib/main.dart
@@ -1,6 +1,7 @@
 import "package:flutter/material.dart";
 import "package:google_fonts/google_fonts.dart";
 import "dream_place_list.dart";
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final baseTheme = ThemeData.light();
 
@@ -38,7 +39,11 @@ final ThemeData customLightTheme = baseTheme.copyWith(
 );
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
