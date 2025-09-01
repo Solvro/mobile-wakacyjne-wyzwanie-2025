a346258d881c7a49503d45095243d78ed741b37a
Mashiro-jz <a.wiacek@zlotoryja.xyz>
2025-09-01 18:53:12 +0200

    update main.dart to Hive database

--- where_to_go/lib/main.dart
+++ where_to_go/lib/main.dart
@@ -1,7 +1,31 @@
-import "package:flutter/material.dart";
-import "package:flutter_riverpod/flutter_riverpod.dart";
-import "app.dart";
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'models/dream_place.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Hive.initFlutter();
+  Hive.registerAdapter(DreamPlaceAdapter());
+
   runApp(const ProviderScope(child: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Dream Places',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      home: const Scaffold(
+        body: Center(child: Text("Dream Places App 🚀")),
+      ),
+    );
+  }
 }
